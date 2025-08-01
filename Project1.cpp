40748a03886fd5c31059f5cd9bf4f542155d3b01
T5AUCE <liudyl323@gmail.com>
2025-08-01 18:01:14 -0400

    Code changed to work with CMake

---
+++ Project1.cpp
@@ -0,0 +1,58 @@
+﻿// Project1.cpp : Defines the entry point for the application.
+//
+
+#include "Project1.h"
+
+/******************************
+* BASE CLASS (WITH PROTECTED) *
+******************************/
+class MyClass {
+public:
+	void myFunction() {
+		std::cout << "Some content in parent class.\n";
+	}
+protected:
+	int some = 5;
+};
+
+// Another base class  
+class MyOtherClass {
+public:
+	void myOtherFunction() {
+		std::cout << "Some content in another class.\n";
+	}
+};
+
+/*************************
+* MULTILEVEL INHERITANCE *
+*************************/
+// Derived class (child)  
+class MyChild : public MyClass {
+};
+
+// Derived class (grandchild)  
+class MyGrandChild : public MyChild {
+public:
+	int getNum() {
+		return some;
+	}
+};
+
+
+/***********************
+* MULTIPLE INHERITANCE *
+***********************/
+// Derived class  
+class MyChildClass : public MyClass, public MyOtherClass {
+};
+
+int main() {
+	MyGrandChild myObj;
+	myObj.myFunction(); // -> Some content in parent class.
+	std::cout << myObj.getNum() << "\n"; // -> 5
+
+	MyChildClass newObj;
+	newObj.myFunction(); // -> Some content in parent class.
+	newObj.myOtherFunction(); // ->  Some content in another class.
+	return 0;
+}
